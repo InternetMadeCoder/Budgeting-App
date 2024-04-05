a4afac25cb03bceab83346021db53d1dd0226d69
Garv Kundnani <garvkundnaniyt@gmail.com>
2024-04-05 22:53:29 +0530

    progress bar

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ import Card from "./Card.js";
 
 const expensesDiv = document.querySelector("#expenses");
 
+const addBudget = (budgetName, spent, total, id) => {
+	expensesDiv.innerHTML += Card(budgetName, spent, total, id)[0];
+	document.getElementById(`progressComplete${id}`).style.width = `${Card(budgetName, spent, total, id)[1]}%`;
+	document.getElementById(`progressLeft${id}`).style.width = `${100 - Card(budgetName, spent, total, id)[1]}%`;
+};
 
-expensesDiv.innerHTML += Card("nashe", 200, 8000);
+addBudget("nashe", 400, 2000, 1);
+addBudget("food", 800, 2500, 2);
+addBudget("groceries", 1000, 1500, 3);
